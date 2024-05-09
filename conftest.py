6661c8d5de4a44c7966f5c5a5804e07b292d6d31
KateVarg <katye9470@gmail.com>
2024-05-09 20:57:05 +0200

    change conftest.py and test_search.py

--- conftest.py
+++ conftest.py
@@ -1,13 +1,13 @@
 import pytest
-
 from selene import browser
 
+
 @pytest.fixture(scope="session", autouse=True)
-def Browser():
+def browser_size():
     browser.config.window_height = 1080
     browser.config.window_width = 680
     print("Изменение размера окна браузера!")
 
     yield
-
+    browser.quit()
     print("Закрываем браузер!")
--- test_search.py
+++ test_search.py
@@ -1,10 +1,13 @@
 from selene import browser, be, have
+
+
 def test_right_search():
-    browser.open('http://google.com')
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('Площадь треугольника').press_enter()
     browser.element('[id="search"]').should(have.no.text('площадь квадрата равна'))
 
+
 def test_wrong_search():
-    browser.open('http://google.com')
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('DFGGDSFdgdffgh').press_enter()
-    browser.element('[id = "search"]').should(have.text('Похоже, по вашему запросу нет полезных результатов'))
+    browser.element('[id = "botstuff"]').should(have.text('По запросу DFGGDSFdgdffgh ничего не найдено'))
